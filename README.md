ac2ed5136a25aeb87902e3d2bfe360ac6cfc0979
sspiff <sspiff@users.noreply.github.com>
2022-03-12 15:41:39 -0500

    template-loader

---
+++ README.md
@@ -0,0 +1,97 @@
+# webpack template-loader
+webpack loader that renders output using lodash.template.
+
+
+## Installation
+
+If not already configured, add the registry for the `@sspiff` package scope
+to the npm configuration:
+
+```
+$ npm config set @sspiff:registry https://npm.pkg.github.com/sspiff -L project
+```
+
+Install the package:
+
+```
+$ npm install -D @sspiff/template-loader
+```
+
+
+## Overview
+
+`template-loader` is a webpack loader that transforms its input using
+[lodash.template](https://lodash.com/docs/#template) with the help of a webpack
+plugin.  It can serve as an alternative to (although not a drop-in replacement
+for) `html-webpack-plugin`, for example, or used to generate JSON from a
+template.
+
+`template-loader` operates in two phases: First as a loader in which it
+generates javascript that, when executed, will output the results of passing
+its input through lodash.template.  Then as a webpack plugin that will execute
+the javascript to produce the rendered output.  *Because of this two-phase
+approach, it must be the last applied (i.e. the first listed) loader in the
+chain.*
+
+When compiling a template, `template-loader` makes two functions available
+for the template to use: `requireResource()` and `requireEntry()`.
+
+`requireResource()` will return the webpack output path for static resources
+(e.g. `asset/resource` type modules).  Use it, for example, in JSON templates
+to reference images or other assets.
+
+`requireEntry()` will return the webpack output path for entry-type modules.
+It takes as input the chunk name as defined in the entry descriptor in
+the webpack configuration.  Modules referenced using `requireEntry()` should
+generally be defined as entry modules in the webpack config.  Use it, for
+example, in an HTML template to reference a webpack-built javascript bundle.
+
+
+## Basic Usage
+
+In `webpack.config.js`:
+
+```
+const TemplateRenderPlugin = require('@sspiff/template-loader').renderPlugin
+
+module.exports = {
+  entry: {
+    indexhtml: {
+      import: 'template!=!./src/index.html',
+      filename: 'index.html',
+    },
+    bundle: {
+      import: './src/index.js',
+      filename: 'bundle.js',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /^template$/,
+        loader: '@sspiff/template-loader',
+      },
+      {
+        test: /\.png$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new TemplateRenderPlugin(),
+  ],
+}
+```
+
+And somewhere in `src/index.html`:
+
+```
+<head>
+  <link rel="icon" href="<%= requireResource('./src/favicon.png') %>" />
+</head>
+
+<body>
+  <script src="<%= requireEntry('bundle') %>"></script>
+</body>
+```
+
---
+++ src/index.js
@@ -0,0 +1,152 @@
+
+const path = require('path')
+const vm = require('vm')
+
+const lodashTemplateResolved = require.resolve('lodash.template')
+const template = require(lodashTemplateResolved)
+
+
+function loader(source) {
+
+  // find our chunk name
+  const [chunkName] = [...this._compilation.entries.entries()]
+    .find(([n, e]) => e.dependencies.some(d =>
+      this.resource === this.utils.absolutify(this.context,
+        './'+path.basename(d.userRequest.split('!').slice(-1)[0]))))
+
+  const resources = []
+  function requireResource(request) {
+    resources.push(request)
+    return ''
+  }
+
+  // make a pass through the template to pick up the requires
+  const compiled = template(source)
+  compiled({
+      requireResource,
+      requireEntry: () => '',
+      T: this._compilation.TemplateRenderPlugin._getParameters(chunkName, 'loader'),
+   })
+
+  // use webpack's own require to get urls to asset resources
+  const requireResourcesCode = resources.map(r =>
+      `resources['${r}'] = require('${r}')`
+    ).join('\n')
+
+  return `
+const template = require('${lodashTemplateResolved}')
+const source = ${JSON.stringify(source)}
+const compiled = template(source)
+
+const resources = {}
+${requireResourcesCode}
+
+T.__rendering = compiled({
+    requireResource: r => resources[r],
+    requireEntry: n => T.__entryMap[n],
+  })
+
+`
+}
+
+
+const STAGE_BASIC = -10    // webpack doesn't export this?
+
+class TemplateRenderPlugin {
+  constructor(options) {
+    this._options = {
+      getParameters: () => ({}),
+      ...options,
+    }
+  }
+
+  _getParameters(chunkName, phase) {
+    return this._options.getParameters({
+      chunkName,
+      phase,
+      compilation: this._compilation,
+    })
+  }
+
+  apply(compiler) {
+
+    compiler.hooks.compilation.tap('TemplateRenderPlugin', (compilation) => {
+      compilation.TemplateRenderPlugin = this
+      this._compilation = compilation
+      compilation.hooks.optimizeChunks.tap(
+        {
+          name: 'TemplateRenderPlugin',
+          stage: STAGE_BASIC,
+        },
+        chunks => {
+          // find all chunks that used our loader
+          const cg = compilation.chunkGraph
+          const templateChunks = [...compilation.chunks]
+            .filter(c => cg.getChunkModules(c).some(m =>
+              m.request && m.request.startsWith(__filename + '!')))
+
+          // remove webpack related entry modules from our template
+          // chunks so that we can render the template without dev-server
+          // bits
+          templateChunks.forEach(c =>
+            [...cg.getChunkEntryModulesIterable(c)]
+            .filter(m => m.request.match(/\/node_modules\/webpack/))
+            .forEach(m => {
+               cg.disconnectChunkAndEntryModule(c, m)
+            })
+          )
+        }
+      )
+    })
+
+    compiler.hooks.thisCompilation.tap('TemplateRenderPlugin', (compilation) => {
+      compilation.hooks.processAssets.tap({
+        name: 'TemplateRender',
+        stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
+      }, () => {
+        // build mapping of chunks to filenames
+        const entryMapPairs = [...compilation.chunks]
+          .map(c => ([c.name, [...c.files.values()][0]]))
+          .filter(([n, f]) => !!n)
+
+        // find all chunks that used our loader
+        const templateChunks = [...compilation.chunks]
+          .filter(c => compilation.chunkGraph.getChunkModules(c).some(m =>
+            m.request && m.request.startsWith(__filename + '!')))
+
+        // get the asset objects for each template-driven chunk
+        const templateAssets = templateChunks
+          .map(c => [c.name, [...c.files.values()][0]])
+
+        // run each render script and replace the asset with the results
+        templateAssets.forEach(([chunkName, assetName]) => {
+          const file = compilation.getAsset(assetName)
+          const renderContext = {
+              T: {
+                __self: chunkName,
+                __entryMap: Object.fromEntries(entryMapPairs.map(([n, f]) => (
+                  [n, path.relative(path.dirname(assetName), f)]))),
+                __rendering: undefined,
+                ...this._getParameters(chunkName, 'render'),
+              },
+              // some webpack boilerplate needs these:
+              self: { location: {} },
+              document: {
+                currentScript: { src: `./${path.basename(assetName)}` },
+              },
+            }
+          const script = new vm.Script(file.source.source())
+          vm.createContext(renderContext)
+          script.runInContext(renderContext)
+          compilation.updateAsset(assetName,
+            new compiler.webpack.sources.RawSource(renderContext.T.__rendering))
+        })
+      })
+    })
+  }
+}
+loader.renderPlugin = TemplateRenderPlugin
+
+
+module.exports = loader
+
